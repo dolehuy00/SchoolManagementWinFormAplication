878f9080acdfce49a7708775d9fcd44a24546ebd
PC <PC@MSI>
2024-01-20 20:23:44 +0700

    update new version

--- src/DAL/GiangdayDAL.java
+++ src/DAL/GiangdayDAL.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAL;
 
 import BLL.GiangDayDTO;
@@ -14,10 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JComboBox;
 
-/**
- *
- * @author DELL
- */
 public class GiangdayDAL {
 
     GiangDayDTO gd = new GiangDayDTO();
